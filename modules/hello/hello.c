5ee403f0b0d9fec70aa9edb2f8690172515b6d5c
bluepick3 <45897349+bluepick3@users.noreply.github.com>
2018-12-31 23:10:59 +0900

    Add files via upload

---
+++ modules/hello/hello.c
@@ -0,0 +1,27 @@
+#include <linux/init.h>
+#include <linux/kernel.h>
+#include <linux/module.h>
+
+//initialization routine
+int __init init_hello(void)
+{
+  printk( KERN_ALERT "[ Hello Module :] Hello, World!.\n" );
+
+
+
+  return 0;
+}
+
+
+//exit routine
+void __exit exit_hello(void)
+{
+  printk( KERN_ALERT "[ Hello Module :] Exit.\n");
+}
+
+
+
+module_init(init_hello);
+module_exit(exit_hello);
+
+
